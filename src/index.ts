9b3e5c53a33cd8bd40fbdf6dd6896dd5af6e2713
jjlmoya <juanjo1989@gmail.com>
2026-04-18 21:28:56 +0200

    fix: remove re-exports of removed component vars from main index

--- src/index.ts
+++ src/index.ts
@@ -17,11 +17,11 @@ export type {
 
 export { ALL_ENTRIES, ALL_TOOLS } from './tools';
 
-export { ChiSquareComponent, ChiSquareSEO, ChiSquareBibliography, CHI_SQUARE_TOOL } from './tool/chiSquare/index';
-export { ABTestComponent, ABTestSEO, ABTestBibliography, AB_TEST_TOOL } from './tool/abTest/index';
-export { SampleSizeComponent, SampleSizeSEO, SampleSizeBibliography, SAMPLE_SIZE_TOOL } from './tool/sampleSize/index';
-export { PearsonCorrelationComponent, PearsonCorrelationSEO, PearsonCorrelationBibliography, PEARSON_CORRELATION_TOOL } from './tool/pearsonCorrelation/index';
-export { DescriptiveStatsComponent, DescriptiveStatsSEO, DescriptiveStatsBibliography, DESCRIPTIVE_STATS_TOOL } from './tool/descriptiveStats/index';
-export { NormalDistributionComponent, NormalDistributionSEO, NormalDistributionBibliography, NORMAL_DISTRIBUTION_TOOL } from './tool/normalDistribution/index';
-export { ConfidenceIntervalComponent, ConfidenceIntervalSEO, ConfidenceIntervalBibliography, CONFIDENCE_INTERVAL_TOOL } from './tool/confidenceInterval/index';
+export { CHI_SQUARE_TOOL } from './tool/chiSquare/index';
+export { AB_TEST_TOOL } from './tool/abTest/index';
+export { SAMPLE_SIZE_TOOL } from './tool/sampleSize/index';
+export { PEARSON_CORRELATION_TOOL } from './tool/pearsonCorrelation/index';
+export { DESCRIPTIVE_STATS_TOOL } from './tool/descriptiveStats/index';
+export { NORMAL_DISTRIBUTION_TOOL } from './tool/normalDistribution/index';
+export { CONFIDENCE_INTERVAL_TOOL } from './tool/confidenceInterval/index';
 
